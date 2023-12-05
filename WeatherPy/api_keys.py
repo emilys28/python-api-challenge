39d90beb136f1f4e826910c46b25fc038c2653cc
emilys28 <esims28.97@gmail.com>
2023-12-05 17:49:31 -0500

    added api key ids

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "151fa341cf4d7522b1a52329d21ca1e1"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "0f335b354ad149c1bfa90332f8e056e5"
